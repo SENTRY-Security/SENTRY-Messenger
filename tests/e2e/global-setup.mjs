ac646a9c18c2c57a795be428193bf191ce0cc65b
SENTRY Dev <dev@users.noreply.github.com>
2025-12-06 17:00:11 +0000

    chore: remove legacy mjs test suites

--- tests/e2e/global-setup.mjs
+++
@@ -1,6 +0,0 @@
-import { clearE2EArtifacts, ensureDir, E2E_ARTIFACT_DIR } from './utils.mjs';
-
-export default async function globalSetup() {
-  await clearE2EArtifacts();
-  await ensureDir(E2E_ARTIFACT_DIR);
-}
