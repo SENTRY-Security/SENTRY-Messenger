cb826d0f8862a02494a32f580aa740fdfad60863
許迺赫 <xunaihe@xunaihedebijixingdiannao-8.local>
2026-02-07 06:17:12 +0800

    Deploy: Sat Feb  7 06:17:12 CST 2026

--- web/src/app/features/messages-support/receipt-store.js
+++ web/src/app/features/messages-support/receipt-store.js
@@ -1,5 +1,6 @@
 // Narrow store for receipt tracking; legacy implementation lives in messages.js.
 
 import { recordMessageRead, recordMessageDelivered, resetReceiptStore } from '../messages.js';
+import { maybeSendDeliveryReceipt } from '../messages/receipts.js';
 
-export { recordMessageRead, recordMessageDelivered, resetReceiptStore };
+export { recordMessageRead, recordMessageDelivered, resetReceiptStore, maybeSendDeliveryReceipt };
